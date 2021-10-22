d28abf07895a7de827ec50350ef47fac4f4ea3bb
Matheus Da Silva <85530101+Matth998@users.noreply.github.com>
2021-10-22 16:17:49 -0300

    Projeto Clone Netflix
    Clone da interface da netflix

---
+++ js/slider.js
@@ -0,0 +1,25 @@
+$(".destaque-slider").slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 7,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 2
+        }
+      }
+    ]
+  });
